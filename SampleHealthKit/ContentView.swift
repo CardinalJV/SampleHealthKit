ad72772e8e116be887f40e65505f204aca60512d
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2025-11-20 21:05:31 +0100

    Initial Commit

---
+++ SampleHealthKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleHealthKit
+//
+//  Created by Viranaiken Jessy on 20/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleHealthKit/SampleHealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleHealthKitApp.swift
+//  SampleHealthKit
+//
+//  Created by Viranaiken Jessy on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleHealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
